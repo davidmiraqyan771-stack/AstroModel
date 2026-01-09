410a698555e753f7a107a161292d02f2632f32fa
davidmiraqyan771-stack <davidmiraqyan771@gmail.com>
2026-01-09 10:47:16 +0400

    Erased useless tdvx tdvy

--- src/physic/Gravity/gravitySolver.c
+++ src/physic/Gravity/gravitySolver.c
@@ -7,10 +7,10 @@ void computeDerivative(const ParticleSystem *src, DerivSystem *dest)
     for (int i = 0; i < src->n; ++i)
     {
         dest->dp[i].dx = src->p[i].v;
-        double tdvx = 0;
-        double tdvy = 0;
+        dest->dp[i].dv.x = 0;
+        dest->dp[i].dv.y = 0;
         for (int j = 0; j < src->n; ++j)
-        { 
+        {
             if (i == j)
             {
                 continue;
@@ -18,10 +18,8 @@ void computeDerivative(const ParticleSystem *src, DerivSystem *dest)
 
             Vec2 subtract = vecSub(src->p[j].x, src->p[i].x);
             double distancesq = vecDistSq(src->p[j].x, src->p[i].x);
-            tdvx += G_CONSTANT * src->p[j].m * (subtract.x * (1.0 / (distancesq * sqrt(distancesq))));
-            tdvy += G_CONSTANT * src->p[j].m * (subtract.y * (1.0 / (distancesq * sqrt(distancesq))));
+            dest->dp[i].dv.x += G_CONSTANT * src->p[j].m * (subtract.x * (1.0 / (distancesq * sqrt(distancesq))));
+            dest->dp[i].dv.y += G_CONSTANT * src->p[j].m * (subtract.y * (1.0 / (distancesq * sqrt(distancesq))));
         }
-        dest->dp[i].dv.x = tdvx;
-        dest->dp[i].dv.y = tdvy;
     }
 }
